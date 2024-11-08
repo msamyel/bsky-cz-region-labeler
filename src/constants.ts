2b3960316dab04b547e64b193b7563974c312d12
alice <aliceisjustplaying@gmail.com>
2024-11-08 19:12:10 +0000

    Initial commit.

---
+++ src/constants.ts
@@ -0,0 +1,83 @@
+import { Label } from './types.js';
+
+export const DELETE = 'insert-rkey-of-delete-post-here';
+export const LABEL_LIMIT = 2;
+export const LABELS: Label[] = [
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'debut',
+    locales: [
+      { lang: 'en', name: 'Debut 🤠', description: 'Debut era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'fearless',
+    locales: [
+      { lang: 'en', name: 'Fearless 👱‍♀️', description: 'Fearless era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here', 
+    identifier: 'speaknow',
+    locales: [
+      { lang: 'en', name: 'Speak Now 💜', description: 'Speak Now era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'red',
+    locales: [
+      { lang: 'en', name: 'Red 🧣', description: 'Red era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: '1989',
+    locales: [
+      { lang: 'en', name: '1989 🌊', description: '1989 era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'reputation',
+    locales: [
+      { lang: 'en', name: 'Reputation 🐍', description: 'Reputation era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'lover',
+    locales: [
+      { lang: 'en', name: 'Lover 💗', description: 'Lover era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'folklore',
+    locales: [
+      { lang: 'en', name: 'folklore 🌲', description: 'folklore era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'evermore',
+    locales: [
+      { lang: 'en', name: 'evermore 🍂', description: 'evermore era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'midnights',
+    locales: [
+      { lang: 'en', name: 'Midnights ✨', description: 'Midnights era'},
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'ttpd',
+    locales: [
+      { lang: 'en', name: 'The Tortured Poets Department 📚', description: 'The Tortured Poets Department'},
+    ]
+  },
+];
